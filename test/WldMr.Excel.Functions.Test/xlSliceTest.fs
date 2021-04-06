44a9fbcc08075f3f3005ccbdecefc5b57242a4c5
PierreYvesR <py.rivaille@gmail.com>
2021-04-06 13:36:09 +0100

    xlSlice: more tests, more fixes

--- test/WldMr.Excel.Functions.Test/xlSliceTest.fs
+++ test/WldMr.Excel.Functions.Test/xlSliceTest.fs
@@ -45,3 +45,18 @@ type ``xlSlice``() =
     |> SubRange.xlSlice
     |> Test.returnedAnArray
     |> Array2D.shouldEqual a33.[1..1, 1..1]
+
+  [<Test>]
+  member __.``3, 4, 1, -2``() =
+    let a56 =
+      [
+        [11; 12; 13; 14; 15; 16]
+        [21; 22; 23; 24; 25; 26]
+        [31; 32; 33; 34; 35; 36]
+        [41; 42; 43; 44; 45; 46]
+        [51; 52; 53; 54; 55; 56]
+      ] |>> (map box) |> array2D
+    (a56, 3.0, 4.0, 1.0, -2.0)
+    |> SubRange.xlSlice
+    |> Test.returnedAnArray
+    |> Array2D.shouldEqual a56.[2..3, 0..4]
