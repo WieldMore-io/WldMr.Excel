d9d76378d9f4fd5c69c6d03b1c31e71edccefb94
PierreYvesR <py.rivaille@gmail.com>
2021-08-10 14:02:45 +0100

    Upgrade ExcelDna to 1.5rc1, msbuild should no longer be required to build the solution Finish removing FSharpPlus Cleanup azure pipeline and solution

--- src/WldMr.Excel.Functions/xlRange.fs
+++ src/WldMr.Excel.Functions/xlRange.fs
@@ -1,9 +1,10 @@
 module WldMr.Excel.SubRange
 
+open WldMr
 open WldMr.Excel.Utilities
 open FsToolkit.ErrorHandling
-open FSharpPlus
 open ExcelDna.Integration
+open WldMr
 
 
 let boolOptionFold f zero bos =
@@ -15,18 +16,18 @@ let boolOptionFold f zero bos =
 
 let internal_and rows cols (rs: (Result<bool option, string list>)[,] list) =
   Array2D.init rows cols
-    (fun i j -> rs |> List.map (fun r -> r.[i, j] |> Result.get) |> boolOptionFold (&&) true)
+    (fun i j -> rs |> List.map (fun r -> r.[i, j] |> Result.value) |> boolOptionFold (&&) true)
 
 
 [<ExcelFunction(Category= "WldMr.Range", Description= "Element-wise boolean AND for arrays")>]
 let xlRangeAnd (range1:obj[,], range2: obj[,], range3: obj[,], range4: obj[,]) =
   let internalAnd rows cols (rs: (Result<bool option, string list>)[,] list) =
     Array2D.init rows cols
-      (fun i j -> rs |> List.map (fun r -> r.[i, j] |> Result.get) |> boolOptionFold (&&) true)
+      (fun i j -> rs |> List.map (fun r -> r.[i, j] |> Result.value) |> boolOptionFold (&&) true)
   let optionRange = function ExcelMissingRange _ -> None | r -> r |> Some
-  let rngs = [range1; range2; range3; range4] |>> optionRange |> List.choose id
+  let rngs = [range1; range2; range3; range4] |> List.map optionRange |> List.choose id
   let res = validation {
-    let! headRng = rngs |> List.tryHead |> Option.toResult |> Result.withError ["xlRangeOr needs at least one parameter"]
+    let! headRng = rngs |> List.tryHead |> Option.toResult id |> Result.withError ["xlRangeOr needs at least one parameter"]
     let size = XlObj.getSize headRng
     do! rngs |> List.forall (XlObj.getSize >> (=) size) |> Result.requireTrue ["All ranges must have the same size"]
     return
@@ -43,11 +44,11 @@ let xlRangeAnd (range1:obj[,], range2: obj[,], range3: obj[,], range4: obj[,]) =
 let xlRangeOr (range1:obj[,], range2: obj[,], range3: obj[,], range4: obj[,]) =
   let internalOr rows cols (rs: (Result<bool option, string list>)[,] list) =
     Array2D.init rows cols
-      (fun i j -> rs |> List.map (fun r -> r.[i, j] |> Result.get) |> boolOptionFold (||) false)
+      (fun i j -> rs |> List.map (fun r -> r.[i, j] |> Result.value) |> boolOptionFold (||) false)
   let optionRange = function ExcelMissingRange _ -> None | r -> r |> Some
-  let rngs = [range1; range2; range3; range4] |>> optionRange |> List.choose id
+  let rngs = [range1; range2; range3; range4] |> List.map optionRange |> List.choose id
   let res = validation {
-    let! headRng = rngs |> List.tryHead |> Option.toResult |> Result.withError ["xlRangeOr needs at least one parameter"]
+    let! headRng = rngs |> List.tryHead |> Option.toResult id |> Result.withError ["xlRangeOr needs at least one parameter"]
     let size = XlObj.getSize headRng
     do! rngs |> List.forall (XlObj.getSize >> (=) size) |> Result.requireTrue ["All ranges must have the same size"]
     return
---
+++ src/WldMr.Excel.Utilities/StringExtensions.fs
@@ -0,0 +1,8 @@
+namespace WldMr
+
+module String =
+  let inline startsWith (prefix: string) (s: string) = s.StartsWith prefix
+  let inline toUpper (s: string) = s.ToUpper()
+  let inline toLower (s: string) = s.ToLower()
+  let inline contains (sub: string) (s: string) = s.Contains sub
+  let inline isEmpty (s: string) = s.Length = 0
