59fcc09d6aeea157d4e72b4bfdc5f14269553321
PierreYvesR <py.rivaille@gmail.com>
2021-12-04 23:16:59 +0000

    xlFormatA, xlSlice: doc, code style, ...

--- src/WldMr.Excel.Functions/xlFormat.fs
+++ src/WldMr.Excel.Functions/xlFormat.fs
@@ -7,13 +7,19 @@ open WldMr.Excel.Functions
 open WldMr.Excel.Utilities
 
 
+// the usability of this function is not great
+// - error reporting is not up to par
+// - it would natural to pass an array of values and an array of types
+// - or maybe to arrayfy the function on inputs
 [<ExcelFunction(Category = "WldMr Text",
                 Description = """Formats an interpolated string as in the .Net world"
-\teg xlFormat("dd-mmm-yy", A1, "d")
+eg xlFormat("{0:dd-mmm-yy}", A1, "d")
+eg xlFormat("{0:0.00}", A1, "f")
 See help link for more details about the syntax""",
                 HelpTopic = "https://docs.microsoft.com/en-us/dotnet/standard/base-types/formatting-types")>]
 let xlFormatA
   (
+    [<ExcelArgument(Description="Format string")>]
     s: string,
     o1: objCell, t1: objCell,
     o2: objCell, t2: objCell,
@@ -23,7 +29,7 @@ let xlFormatA
     o6: objCell, t6: objCell,
     o7: objCell, t7: objCell,
     o8: objCell, t8: objCell
-  ) =
+  ): objCell =
   let convertXlObj (o: objCell) (t: objCell) =
     match o, t with
     | ExcelMissing _, ExcelMissing _ -> "" :> obj |> Ok
@@ -35,20 +41,22 @@ let xlFormatA
         | Ok "i" -> o |> XlObj.toFloat |> Result.map (int >> box)
         | Ok "f" -> o |> XlObj.toFloat |> Result.map box
         | Ok "s" -> o |> XlObj.toString |> Result.map box
-        | _ ->
-            "Invalid format specifier. Use d, i, f, or s."
-            |> Error
-        |> Validation.ofResult
+        | Ok x -> $"Invalid format specifier '{x}'. Use d, i, f, or s." |> Error
+        | Error e -> $"Invalid format specifier. Use d, i, f, or s. {e}" |> Error
 
-  validation {
+  let formatString s args =
+    try
+      System.String.Format(s, List.toArray args) |> XlObj.ofString |> Ok
+    with
+      | :? System.FormatException as e -> "Incorrect format string." |> Error
+      | e -> $"{e.Message} ({e.GetType()})" |> Error
+
+  result {
     let! args =
       ([ o1; o2; o3; o4; o5; o6; o7; o8 ],
        [ t1; t2; t3; t4; t5; t6; t7; t8 ])
       ||> List.map2 convertXlObj
-      |> List.sequenceValidationA
-      |> Result.map List.toArray
+      |> List.sequenceResultM
 
-    return! (s, args)
-      |> Result.protect (System.String.Format >> box >> (~%))
-      |> Result.mapError (fun err -> [ $"{err}" ])
-  } |> XlObj.ofValidation
+    return! formatString s args
+  } |> XlObj.ofResult
--- src/WldMr.Excel.Functions/xlSlice.fs
+++ src/WldMr.Excel.Functions/xlSlice.fs
@@ -5,20 +5,10 @@ open FsToolkit.ErrorHandling
 open ExcelDna.Integration
 
 
-let parseArg parseF t (errMap: string -> string) (xlObj: objCell) =
-  match xlObj with
-  | ExcelMissing _
-  | ExcelEmpty _
-  | ExcelString "" -> t |> Ok
-  | _ ->
-      xlObj
-      |> parseF
-      |> Result.mapError (errMap >> fun x -> [x])
-
-
 [<ExcelFunction(Category= "WldMr Array",
   Description=
     "Selects a subrange of an array.\r\n" +
+    "A variation on the built-in 'Offset' function.\r\n" +
     "The arguments use the following convention:\r\n" +
     "  1   means the 1st row/column\r\n" +
     "       ... \r\n" +
@@ -37,28 +27,23 @@ let xlSlice
       fromColumn: objCell,
     [<ExcelArgument(Description="the last column to return, defaults to -1")>]
       toColumn: objCell
-  ): objCell[,] =
-  let res = validation {
-    let! base1_sr = fromRow |> parseArg XlObj.toInt 1 (fun e -> $"arg 'FromRow': {e}" )
-    and! base1_er = toRow |> parseArg XlObj.toInt -1 (fun e -> $"arg 'ToRow': {e}" )
-    and! base1_sc = fromColumn |> parseArg XlObj.toInt 1 (fun e -> $"arg 'FromColumn': {e}" )
-    and! base1_ec = toColumn |> parseArg XlObj.toInt -1 (fun e -> $"arg 'ToColumn': {e}" )
-    let sr = base1_sr - 1
-    let sc = base1_sc - 1
-    let er = base1_er - 1
-    let ec = base1_ec - 1
+  ): objCell[,]
+  =
+  result {
     let nRows = range.GetLength 0
     let nCols = range.GetLength 1
-    let startRow = if sr >= 0 then sr else nRows + sr + 1
-    let startCol = if sc >= 0 then sc else nCols + sc + 1
-    let endRow = if er >= 0 then er else nRows + er + 1
-    let endCol = if ec >= 0 then ec else nCols + ec + 1
+    let! sr = fromRow |> (XlObj.argToInt |> XlObj.argDefault 1) "FromRow"
+    and! sc = fromColumn |> (XlObj.argToInt |> XlObj.argDefault 1) "FromColumn"
+    and! er = toRow |> (XlObj.argToInt |> XlObj.argDefault -1) "ToRow"
+    and! ec = toColumn |> (XlObj.argToInt |> XlObj.argDefault -1) "ToColumn"
+    let startRow = sr + if sr >= 0 then -1 else nRows
+    let startCol = sc + if sc >= 0 then -1 else nCols
+    let endRow = er + if er >= 0 then -1 else nRows
+    let endCol = ec + if ec >= 0 then -1 else nCols
     let slice = range.[startRow..endRow, startCol..endCol]
     if slice.LongLength = 0L then
       return XlObj.Error.objNA |> Array2D.create 1 1
     else
       return slice
   }
-  res |> XlObjRange.ofValidation
-
-
+  |> XlObjRange.ofResult
