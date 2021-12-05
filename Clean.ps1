6262d9bdd7f84f201b3e43029eb463f16d2024d6
PierreYvesR <py.rivaille@gmail.com>
2021-12-05 00:08:04 +0000

    Update paket. Improve Clean.ps1

--- Clean.ps1
+++ Clean.ps1
@@ -1,1 +1,4 @@
-gci -recurse . -directory | where name -match "^(bin|obj)$" | select fullname | foreach { rm -rec $_.FullName}
+#!/usr/bin/env pwsh
+remove-item -rec .paket -erroraction silentlycontinue
+remove-item -rec paket-files -erroraction silentlycontinue
+gci -recurse . -directory | where name -match "^(bin|obj)$" | select fullname | foreach { remove-item -rec -force $_.FullName}
