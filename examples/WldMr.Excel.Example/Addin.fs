95d197c27ac4ab54c001f98c7aad453116270fb5
PierreYvesR <py.rivaille@gmail.com>
2022-01-18 23:15:12 +0000

    net6.0 support ExcelDNA 1.6.0-preview Disable ExcelDNA.IntelliSense temporarily

--- examples/WldMr.Excel.Example/Addin.fs
+++ examples/WldMr.Excel.Example/Addin.fs
@@ -1,14 +1,16 @@
 module WldMr.Excel.Example.Addin
 
 open ExcelDna.Integration
-open ExcelDna.IntelliSense
+// open ExcelDna.IntelliSense
 
 
 type AddIn() =
   interface IExcelAddIn with
     member _.AutoOpen() =
-      IntelliSenseServer.Install()
+      // IntelliSenseServer.Install()
+      ()
 
     member _.AutoClose() =
-      IntelliSenseServer.Uninstall()
+      // IntelliSenseServer.Uninstall()
+      ()
   end
