aded3781ff617fd14840bd6e0daa1fdcd9530729
PierreYvesR <py.rivaille@gmail.com>
2022-02-26 13:22:37 +0000

    Array2D.flatten

--- src/WldMr.Excel.Core/FSharpCoreExtensions.fs
+++ src/WldMr.Excel.Core/FSharpCoreExtensions.fs
@@ -4,13 +4,15 @@
 [<RequireQualifiedAccess>]
 module Array2D =
   /// <summary>
-  /// flattens the array as a list of rows, each row being a list
+  /// flattens the array as an array of rows, each row being an array
   /// simplify further processing (although potentially slower)
   /// </summary>
   let flatten array2d =
-    [ for x in [ 0 .. (Array2D.length1 array2d) - 1 ] do
-      [ for y in [ 0 .. (Array2D.length2 array2d) - 1 ] do
-        yield array2d.[x, y] ] ]
+    Array.init (Array2D.length1 array2d)
+      (fun i ->
+        Array.init (Array2D.length2 array2d)
+          (fun j -> array2d.[i, j]))
+
 
 [<RequireQualifiedAccess>]
 module Async =
